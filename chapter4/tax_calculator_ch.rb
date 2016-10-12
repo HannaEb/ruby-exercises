ce2c85d1a064d1216b064f7a425d5f2d742f45a3
Hanna <hannaeberhoefer@yahoo.com>
2016-10-12 17:52:52 +0100

    Tax Calculator

---
+++ chapter4/tax_calculator_ch.rb
@@ -0,0 +1,14 @@
+puts 'What is the order amount?'
+amount = gets.chomp.to_f
+
+puts 'What is the state?'
+state = gets.chomp
+
+tax = 0.55
+
+if state == 'WI'
+  puts "The subtotal is £#{amount}\nThe tax is £#{tax}"
+  amount = amount + tax
+end
+
+puts "The total is £#{amount}"
